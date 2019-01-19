90015858e51a9eac888327008d852d698cc3d14c
Sare Soltani <sare.soltani96@yahoo.com>
2019-01-20 01:20:37 +0330

    added Split func

--- BalancedQueue.py
+++ BalancedQueue.py
@@ -1,13 +1,19 @@
+from SearchAlgorithms.HillClimbing import *
 import random
 from copy import deepcopy
 import numpy as np
 
+def split_list(alist, wanted_parts):
+    length = len(alist)
+    return [ alist[i*length // wanted_parts: (i+1)*length // wanted_parts] for i in range(wanted_parts) ]
+
 class Problem():
     def initialState(self):
        k = 2
-       #queue= np.random.randint(1,10,3*k)
-       queue = [2, 3, 6, 9, 7, 2]
-       print(queue)
+       #queue= np.random.randint(1,10,6)
+       queue = [2, 8, 6, 5, 2, 0]
+       #print(queue)
+       return queue
 
 p = Problem()
 p.initialState()
